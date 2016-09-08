2612516a060a5197d0d3a7acfe24c1bf8a05760b
Oleg Zhukov <jukovoleg@gmail.com>
2016-09-08 11:35:47 +0000

    implemented destroy post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,11 @@ def update
   @post.update(post_params)
   redirect_to(post_path(@post))
 end  
+def destroy  
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to posts_path
+end  
 private
 
 def post_params  
