513378e500e4dae2a88714a9ba8edf21cae57bb7
Oleg Zhukov <jukovoleg@gmail.com>
2016-09-29 12:45:25 +0000

    comments added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base  
   validates :user_id, presence: true
+  has_many :comments, dependent: :destroy
   belongs_to :user
   validates :image, presence: true
   has_attached_file :image, styles: { :medium => "640x" }
